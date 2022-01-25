c946b2e3a4feb4dc23b66777e2237c5fbcd8cf63
joaquin sayago <isc.jsayago@hotmail.com>
2022-01-24 19:05:34 -0600

    recuperando datos del servidor con formato xml

---
+++ js/09.recuperandoDatosDelServidorXML.js
@@ -0,0 +1,64 @@
+//console.log("Recuperando los datos del servidor con XML");
+
+let inicializarEventos = function (){
+
+    let obj = document.querySelector("#boton1");
+    obj.addEventListener("click", presionBoton, false);
+}
+
+
+let presionBoton = function (e){
+    let obj = document.querySelector("#pais").value;
+    recuperarDatos(obj);
+}
+
+
+let recuperarDatos = function (pais){
+
+    conexion = new XMLHttpRequest();
+    conexion.onreadystatechange = procesarEventos;
+    conexion.open('GET', `./php/09.recuperandoDatosDelServidorXML.php?pa=${pais}`, true)
+    conexion.send();
+}
+
+
+let procesarEventos = function (){
+
+    let resultados = document.querySelector("#resultados");
+
+    //console.log(conexion.readyState);
+
+    if(conexion.readyState == 4){
+
+        let xml = conexion.responseXML;
+
+        if(xml != '0'){
+
+            let capital = xml.getElementsByTagName("capital");
+            let superficie = xml.getElementsByTagName("superficie");
+            let idioma = xml.getElementsByTagName("idioma");
+            let poblacion = xml.getElementsByTagName("poblacion");
+            
+            resultados.innerHTML = `Capital: ${capital[0].firstChild.nodeValue} <br>
+                                    Superficie: ${superficie[0].firstChild.nodeValue} <br>
+                                    Idioma: ${idioma[0].firstChild.nodeValue}<br>
+                                    Población: ${poblacion[0].firstChild.nodeValue}<br>`;
+        
+            
+            resultados.style.display = "block";
+
+        }else {
+
+            console.log("Opción inválida");
+            resultados.innerHTML = 'Opcion inválida';
+            resultados.style.display = "block";
+        }   
+
+    }else{
+
+        resultados.innerHTML = 'Cargando...';
+    }
+}
+
+var conexion;
+addEventListener("load",inicializarEventos,false);
