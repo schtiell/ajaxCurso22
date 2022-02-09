df9f46fdd175c3dac026c94107e4aeedb2d838d5
joaquin sayago <isc.jsayago@hotmail.com>
2022-02-08 18:41:28 -0600

    Agregando comentarios y pequeños cambios al ejercicio 15

--- php/15.paginacionAjax.php
+++ php/15.paginacionAjax.php
@@ -12,40 +12,58 @@
     if (isset($_REQUEST['pos'])) {
 
         $inicio = $_REQUEST['pos'];
+
+        //Llamando al método listarComentarios enviando 2 argumentos
         $array = $consulta->listarComentarios($inicio, 3);
     }else{
 
         $inicio = 0;
+
+        //Llamando al método listarComentarios enviando 2 argumentos
         $array = $consulta->listarComentarios($inicio, 3);
     }
 
-    $impresos = 0;
+    //Variable para contar el numero de registros devueltos por la consulta a la bd mysql
+    $contadorRegistros = $array -> rowCount();
 
-    while($comentario = $array->fetch(PDO::FETCH_ASSOC)){
+    if ($contadorRegistros >= 1){
 
-        echo    "Nombre: "      . $comentario['nombre']     . "<br>"    .                    
-                "Fecha: "       . $comentario['fecha']      . "<br>"    .
-                "Comentario: "  . $comentario['comentarios']."<br>"     .
-                "===================================================="  .
-                "<br>";
-        $impresos+=1;
-    }
+        //3 Segundos de espera para simular la tardanza de respuesta del servidor al front-end
+        sleep(3);
+        
+        //Variable para el conteo de comentarios impresos
+        $impresos = 0;
 
-    if($inicio == 0){
+        while($comentario = $array->fetch(PDO::FETCH_ASSOC)){
 
-        echo "<p class=\"ant\"> Anteriores </p>";
+            echo    "Nombre: "      . $comentario['nombre']     . "<br>"    .                    
+                    "Fecha: "       . $comentario['fecha']      . "<br>"    .
+                    "Comentario: "  . $comentario['comentarios']."<br>"     .
+                    "===================================================="  .
+                    "<br>";
+            $impresos+=1;
+        }
+    
+        if($inicio == 0){
+    
+            echo "<p class=\"ant\"> Anteriores </p>";
+        }else{
+        
+            $anterior = $inicio - 3;
+            echo "<a href=\"./php/15.paginacionAjax.php?pos=$anterior\" class=\"ant\">Anteriores</a>";
+        }
+        
+        if ($impresos == 3){
+        
+            $proximo = $inicio + 3;
+            echo "<a href=\"./php/15.paginacionAjax.php?pos=$proximo\" class=\"sig\">Siguientes</a>";
+        }else{
+        
+            echo "<p class=\"sig\">Siguientes</p>";
+        }
+
     }else{
-    
-        $anterior = $inicio - 3;
-        echo "<a href=\"./php/15.paginacionAjax.php?pos=$anterior\" class=\"ant\">Anteriores</a>";
-    }
-    
-    if ($impresos == 3){
-    
-        $proximo = $inicio + 3;
-        echo "<a href=\"./php/15.paginacionAjax.php?pos=$proximo\" class=\"sig\">Siguientes</a>";
-    }else{
-    
-        echo "<p class=\"sig\">Siguientes</p>";
+
+        echo "No hay comentarios en la base de datos";
     }
 ?>
--- php/config.php
+++ php/config.php
@@ -1,6 +1,14 @@
 <?php
     class Config{
 
+        /*
+        Public: podemos acceder a las propiedades y métodos desde cualquier lugar, desde la clase actual, clases que heredan de la clase actual y desde otras clases.
+        
+        Protected: se puede acceder al atributo o método desde la clase que lo define y desde cualquier otra que herede de esta clase.
+        
+        Private: los atributos o métodos solo son accesibles desde la clase que los define.
+        */
+
         //Atributos protegidos que solo pueden ser accedidos desde la case propia o de clases que hereden de esta.
         protected $host = 'localhost';
         protected $dbname = 'bdajax';
