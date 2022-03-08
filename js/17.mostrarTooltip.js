985baababa4c025d567cf3b16c06233ec98bacc0
jsayagot <isc.jsayago@hotmail.com>
2022-03-08 12:53:18 -0600

    ejercicio terminado

--- js/17.mostrarTooltip.js
+++ js/17.mostrarTooltip.js
@@ -1,84 +1,78 @@
 
-//Funcion inicializarEventos
-let inicializarEventos = function () {
+//Función para inicializar los eventos, al seleccionar los elementos con clase .cuadradito
+function inicializarEventos() {
 
-    //Recuperando todos los elementos con clase cuadradito y almacenados en un array
-    let array = document.querySelectorAll('.cuadradito');
+  var array = document.getElementsByClassName('cuadradito');
 
-    //Recorrido del arragle de los elementos div para la aplicacion de los diferentes eventos
-    for (let i = 0; i < array.length; i++) {
-        
-        array[i] = addEventListener('mouseover', mostrarTooltip, false);
-        array[i] = addEventListener('mouseout', ocultarTooltip, false);
-        array[i] = addEventListener('mousemove', actualizarTooltip, false);       
-    }
-    
-    //Creando el elemento div que será utilizado como tooltip
-    let tooltip = document.createElement('div');
-    tooltip.setAttribute('id', 'divmensaje');   
-    array = document.querySelectorAll('body');
-    array[0].appendChild(tooltip);
+  for(i=0; i<array.length; i++) {
+
+    array[i].addEventListener('mouseover', mostrarToolTip, false);
+    array[i].addEventListener('mouseout', ocultarToolTip, false);
+    array[i].addEventListener('mousemove', actualizarToolTip, false);
+  }
+
+  //Creación de elemento tooltip
+  let tooltip = document.createElement('div');
+  tooltip.setAttribute('id','divmensaje');
+
+  array = document.querySelectorAll('body');
+  array[0].appendChild(tooltip);
+
 }
 
-//Funcion para mostrar el tooltip al posicionar el apuntador del mouse sobre el elemento div
-let mostrarTooltip = function (e) {
-    
-    let divmensaje = document.querySelector('#divmensaje');
-    divmensaje.style.visibility = "visible";
-    divmensaje.style.left = (e.clientX + document.body.scrollLeft + 15)+'px';
-    divmensaje.style.top = (e.clientY + document.body.scrollTop + 15) + 'px';
-    let ref;
-    ref = e.target;
-    recuperarServidorTooltip(ref.getAttribute("id")); 
+//Funcion para mostras el tooltip al pasar el mouse sobre el elemento
+function mostrarToolTip(e){
+
+  let tooltip = document.getElementById("divmensaje");
+  tooltip.style.visibility = "visible";
+  tooltip.style.left = (e.clientX + document.body.scrollLeft + 15)+'px';
+  tooltip.style.top = (e.clientY + document.body.scrollTop + 15)+'px';
+  
+  let referencia;
+  referencia = e.target;
+  recuperarServidorTooltip(referencia.getAttribute('id'));
 }
 
-//Funcion para ocultar el tooltip al posicionar el apuntador del mouse sobre el elemento div
-let ocultarTooltip = function (e) {
-    
-    let divmensaje = document.getElementById("divmensaje");
-    divmensaje.style.visibility = "hidden";
+//Funcion para actualizar la posicion del tooltip deacuerdo al moviento del mouse
+function actualizarToolTip(e) {
+
+  let tooltip = document.getElementById("divmensaje");
+  tooltip.style.left = (e.clientX + document.body.scrollLeft + 15)+'px';
+  tooltip.style.top = (e.clientY + document.body.scrollTop + 15)+'px';
 }
 
-//Funcion para actualizar la posición del tooltip al posicionar el apuntador del mouse sobre el elemento div
-let actualizarTooltip = function (e) {
+//Función para ocultar el tooltip al quitar el mouse del elemento html
+function ocultarToolTip(e){
 
-    let divmensaje = document.getElementById("divmensaje");
-    divmensaje.style.left = (e.clientX + document.body.scrollLeft + 15) + 'px';
-    divmensaje.style.top = (e.clientY + document.body.scrollTop + 15) + 'px';
+  let tooltip = document.getElementById("divmensaje");
+  tooltip.style.visibility = "hidden";
 }
 
-//Funcion ajax para el envío/recepción //Funcion para mostrar el tooltip al posicionar el apuntador del mouse sobre el elemento div
-let recuperarServidorTooltip = function (codigo) {
-    
-    conexion = new XMLHttpRequest();
-    conexion.onreadystatechange = procesarEventos;
-    conexion.open("GET", `../php/17.obtenerTooltip.php?codigo=${codigo}`,true)
-    conexion.send();
+//Función para solicitar la información al servidor deacuerdo al codigo enviado al servidor de manera asíncrona
+function recuperarServidorTooltip(codigo){
+
+  conexion=new XMLHttpRequest();
+  conexion.onreadystatechange = procesarEventos;
+  conexion.open('GET','../php/17.obtenerTooltip.php?codigo='+codigo, true);
+  conexion.send();
 }
 
-//
-let procesarEventos = function () {
-    
-    let divmensaje = document.getElementById("divmensaje");
-    divmensaje.style.visibility = "visible";
+//Función para procesar los 
+function procesarEventos() {
 
-    if (conexion.readyState == 4 ) {
-        
+  let tooltip = document.getElementById("divmensaje");
+  tooltip.style.visibility = "visible";
 
-        if (conexion.status == 200) {
+  if(conexion.readyState == 4) {
 
-            divmensaje.innerHTML = conexion.responseText;
-            
-        } else {
-            
-            divmensaje.innerHTML = `Error: ${conexion.status}`;
-        }
-    } else {
+    tooltip.innerHTML=conexion.responseText;
 
-        divmensaje.innerHTML =  `<img src="../img/loader2.gif">`;
-    }
+  } else {
+
+    tooltip.innerHTML = '<img src="../img/loader2.gif">';
+  }
 }
 
-//Variable de conexion global y evento load para iniciar la funcion inicializarEventos en cuanto el sitio web haya cargado completamente
+//Variable global de conexion y espera del evento load para inicializar los eventos del sitio
 var conexion;
-addEventListener("load", inicializarEventos, false);
+addEventListener('load',inicializarEventos,false);
