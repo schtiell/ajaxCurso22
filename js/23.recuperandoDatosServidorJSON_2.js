e12f059da8411a42ed2646a50f15e6e87390a8c7
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2022-04-10 13:12:05 -0500

    Se comento el codigo y se termino el ejercicio 23

--- js/23.recuperandoDatosServidorJSON_2.js
+++ js/23.recuperandoDatosServidorJSON_2.js
@@ -1,23 +1,23 @@
 
-//Iniciando los eventos
+//Iniciando los eventos al cambiar la opciòn el elemento html select con id #dni
 let iniciarEventos = function () {
 
-    let id = document.querySelector('#dni');
+    let opcion = document.querySelector('#dni');
 
-    id.addEventListener('change', e => {
+    opcion.addEventListener('change', e => {
 
-        id = document.querySelector('#dni').value;
+        opcion = document.querySelector('#dni').value;
 
-        if (id == 0) {
+        if (opcion == 0) {
 
             resultados.innerHTML = 'Debes elegir una opción valida';
         }else{
-            recuperarDatos(id);
+            recuperarDatos(opcion);
         }
     }, false);
 }
 
-//
+//Crea la conexion asincrona con el servidor y le envia por la url el dni contenido en el parametro de la funcion id
 let recuperarDatos = function (id) {
 
     conexion = new XMLHttpRequest();
@@ -26,15 +26,18 @@ let recuperarDatos = function (id) {
     conexion.send();
 }
 
-//
+//Funcion que procesa la respuesta del servidor si el estado de conexion es 4 y el estatus del sitio devuelto es 200
 let procesarEventos = function () {
 
     if (conexion.readyState == 4) {
 
         if(conexion.status == 200) {
 
+            //Analiza la cadena de texto devuelta como JSON y la convierte a javascript
             let datos = JSON.parse(conexion.responseText);
-            resultados.innerHTML = `<strong>    Usuario:    </strong>   ${datos.usuario}<br></br>`;
+
+            //Impresion de la respuesta devuelta por el servidor y formateada por la función crearSalida()
+            resultados.innerHTML = crearSalida(datos.id, datos.nombre, datos.usuario, datos.email, datos.telefono, datos.compania);
 
         } else {
 
@@ -42,11 +45,43 @@ let procesarEventos = function () {
             resultados.innerHTML = "<img src='../img/loader2.gif' />";
         }
     } else {
+        //Imagen de carga mientras el servidor responde a la solicitud
         resultados.innerHTML = "<img src='../img/loader2.gif' />";
     }
 }
 
-//
+//Funcion que crea la cadena de salida de la respuesta devuelta por el servidor
+let crearSalida = function (id, nombre, user, email, tel, compania) {
+
+    //Creacion de tabla que será impresa por la función procesarEventos
+    return  `<table class="table table-striped table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <td>#</td>
+                        <td>Nombre</td>
+                        <td>Usuario</td>
+                        <td>Email</td>
+                        <td>Telefono</td>
+                        <td>Compañia</td>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope='col'>${id}</td>
+                        <td>${nombre}</td>
+                        <td>${user}</td>
+                        <td>${email}</td>
+                        <td>${tel}</td>
+                        <td>${compania}</td>
+                    </tr>
+                </tbody>
+            </table>`;
+}
+
+//Varible conexion y resultados declaradas en el scope global
 var conexion;
 let resultados = document.querySelector('#resultados');
+
+//Inicio de los eventos al cargar el sitio web por completo
 addEventListener('load', iniciarEventos, false);
--- php/23.respuestaJSON_2.php
+++ php/23.respuestaJSON_2.php
@@ -1,21 +1,28 @@
 <?php
 
+    #Importando la clase Usuario y el document root
     require $_SERVER['DOCUMENT_ROOT'] . "/php/Usuario.php";
 
     header('Content-Type: text/txt; charset=utf8');
 
+    #Un segundo antes de procesar la solicitud
     sleep(1);
 
+    #Almacena en variable id el valor del dni enviado mediante la url
     $id = $_REQUEST["dni"];
 
+    #Creación de la instancia usuario
     $usuario = new Usuario();
 
+    #Array declarado para almacenar la información devuelta por el metodo listarUsuarios de la clase usuario
     $array = $usuario -> listarUsuarios($id);
 
-    while($row = $array->fetch(PDO::FETCH_ASSOC)){
-        $datos = $row;
-    }
+    #PDOStatement obtiene la fila de un conjunto de datos. PDO::FETCH_ASSOC: devuelve un array indexado por los nombres de las columnas del conjunto de resultados.
+    $row = $array->fetch(PDO::FETCH_ASSOC);
 
-    $cadena = json_encode($datos);
+    #Se codifica el valor en formato json
+    $cadena = json_encode($row);
+
+    #Se imprime el valor de la variable cadena
     echo $cadena;
 ?>
