1c536f7fba2e8e6abf06b0bf9f17abefb263e7f7
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2022-04-09 23:53:02 -0500

    comentando el codigo de php ejercicio 22

--- php/22.respuestaJSON.php
+++ php/22.respuestaJSON.php
@@ -1,17 +1,23 @@
 <?php
 
+    #Importando la clase Usuario
     require_once $_SERVER['DOCUMENT_ROOT']."/php/Usuario.php";
 
     header('Content-Type: text/txt; charset=utf8');
 
+    #Tiempo de espera de un segundo para devolver una respuesta del servidor
     sleep(1);
 
+    #Almancenado el id enviado por Ajax mediante la url en una variable
     $id = $_REQUEST['dni'];
 
+    #Creando una instancia de la clase usuario
     $usuario = new Usuario();
 
+    #Invocando al metodo listarUsuario de la clase usuario enviando como argumento el id
     $array = $usuario -> listarUsuarios($id);
 
+    #Recorriendo la respuesta y almacenando cada dato en una variable
     while($row = $array->fetch(PDO::FETCH_ASSOC)){
         # code...
         $user = $row['usuario'];
@@ -22,6 +28,7 @@
         $datos = $row;
     }
 
+    #Imprimiendo la respuesta generada por el servidor y devualta al usuario de forma asincrona con ajax
     echo "{
         \"Usuario\":\"$user\",
         \"Nombre\":\"$nombre\",
