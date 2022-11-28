2083c4968bc1bf5d20c52b3e30c15323bddc2623
jsayagot <isc.jsayago@hotmail.com>
2022-11-28 17:42:11 -0600

    XMLHttpRequest objetc

--- js/05.objectXMLHttprequest.js
+++ js/05.objectXMLHttprequest.js
@@ -1,53 +1,42 @@
-console.log("Objeto XMLHttprequest");
+let inicializarEventos = function (){
 
-//La funcion inicializarEventos() obtiene la referencia del boton y al ejecutarse el evento clic sobre este llama a la función presionBoton() 
-function inicializarEventos () {
-    let = objeto = document.querySelector("#boton");
-    objeto.addEventListener("click", presionBoton, false);
+    let boton = document.querySelector('#boton');
+    boton.addEventListener('click', presionarBoton, false);
 }
 
-//La funcion presionBoton() recibe el evento, obtiene la referencia de los elementos con id voto y nombre, para posteriormente enviar como argumentos a la función cargarVoto() el valor de cada referencia
-function presionBoton (e) {
-    let voto = document.querySelector("#voto").value;
-    let nombre = document.querySelector("#nombre").value;
-    cargarVoto(voto, nombre);
+let presionarBoton = function (e) {
+
+    let nombre = document.querySelector('#nombre').value;
+    let voto = document.querySelector('#voto').value;
+
+    cargarDatos(nombre, voto);
 }
 
-//Variable global de conexión
-var conexion;
-
-//La funcion cargarVoto recibe el valor de voto y nombre como parametros, e inicia el proceso de asíncrono de jsS
-function cargarVoto (voto, nombre) {
+let cargarDatos = function (nombre, voto) {
     
-    //Crea el objeto XMLHttpRequest
-    conexion = new XMLHttpRequest();
+    xhttpr = new XMLHttpRequest();
 
-    //La funcion onreadestatechange llama a la función procesarEventos
-    conexion.onreadystatechange = procesarEventos;
+    xhttpr.onreadystatechange = procesarDatos;
 
-    //Se inicia el envío de datos, mediante el metodo GET, la url con las variable a guardar, y el procedimiento true de asincrono
-    conexion.open('GET', `./php/05.objectXMLHttprequest.php?puntaje=${voto}&nombre=${nombre}`, true);
+    xhttpr.open('GET', `../php/05.objectXMLHttprequest.php?puntaje=${voto}&nombre=${nombre}`, true);
 
-    //Envío de los datos
-    conexion.send();
-} 
-
-//La funcion procesarEventos, invoca al elemento html con id resultados
-function procesarEventos (){
-    let resultados = document.querySelector("#resultados");
-    //console.log(conexion.readyState);
-
-    //Si el estado de la conexión es igual a 4 entonces se imprime en el DOM la respuesta del servidor, si no imprime cargando
-    if(conexion.readyState == 4){
-
-        resultados.style.display = "block";
-        resultados.innerHTML = conexion.responseText;
-
-    }else{
-        
-        resultados.innerHTML = "Cargando...";
-    }
+    xhttpr.send();
 }
 
-//Funcion que se ejecuta al cargarse por completo el sitio.
-addEventListener("load", inicializarEventos, false);
+
+let procesarDatos = function () {
+    
+    let impresion = document.querySelector('#resultados');
+
+    if (xhttpr.readyState == 4 && xhttpr.status == 200) {
+        
+        impresion.style.display =  "block";
+        impresion.innerHTML = xhttpr.responseText;
+        
+    } else {
+        impresion.innerHTML = "Cargando...";
+    }  
+}
+
+var xhttpr;
+addEventListener('load', inicializarEventos, false);
--- php/05.objectXMLHttprequest.php
+++ php/05.objectXMLHttprequest.php
@@ -3,17 +3,20 @@
     header('Content-Type: text/html; charset=utf-8');
 
     //Se abre el documento puntaje.txt si no existe lo crea y escribe al final de la ultima linea
-    $file = fopen("../php/puntaje.txt", "a")or die ("1. No se pudo abrir el archivo");
+    $file = fopen("../php/puntaje.txt", "a") or die ("1. No se pudo abrir el archivo para insertar los datos");
+
+    $nombre = $_REQUEST['nombre'];
+    $voto = $_REQUEST['puntaje'];
 
     //Se agregan los valores enviados mediante la url, el nombre y el voto
-    fputs($file, "<p>"."<span>Nombre: </span>".$_REQUEST['nombre']."</p>");
-    fputs($file, "<p>"."<span>Voto: </span>".$_REQUEST['puntaje']."</p>");
+    fputs($file, "<p>"."<span>Nombre: </span>". $nombre ."</p>");
+    fputs($file, "<p>"."<span>Voto: </span>". $voto ."</p>");
     
     //Se cierra el archivo
     fclose($file);
 
      //Se abre el documento puntaje.txt en modo lectura unicamente e inicia su lectura en la primera linea
-    $file = fopen("../php/puntaje.txt", "r")or die ("2. No se pudo abrir el archivo");
+    $file = fopen("../php/puntaje.txt", "r") or die ("2. No se pudo abrir el archivo, para realizar impresión");
 
     //Comprueba si se ha llegado a la ultima linea del archivo
     while (!feof($file)){
